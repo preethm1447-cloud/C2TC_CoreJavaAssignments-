537f2d17456e0325902733b5154bc9ee4a2ba52d
preethm1447-cloud <preethm1447@gmail.com>
2026-01-05 01:42:58 +0530

    basics java

---
+++ Java_tns/src/com/tnsbasics/IdentifiersDemo.java
@@ -0,0 +1,17 @@
+package com.tnsbasics;
+
+public class IdentifiersDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		int $number = 17; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Preethii"; 
+		System.out.println("value is : "+ studentName);
+		
+	
+	}
+
+}
